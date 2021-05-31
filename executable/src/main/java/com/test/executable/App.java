b9c1c1a2e36f1161a36d8f380bd2f23d1de2e479
Kashyap Kansara <kashyap.kansara@uffizio.in>
2021-05-31 16:54:15 +0000

    exception handled

--- executable/src/main/java/com/test/executable/App.java
+++ executable/src/main/java/com/test/executable/App.java
@@ -34,69 +34,72 @@ public class App {
 		String workspaceId = "5f4cf74e5a388d1887a29ab9";
 		String userTimeZone = "Asia/Kolkata";
 		
-		boolean taskRunning;
-		String taskName;
-		String taskLink;
-		JSONArray tasks;
-		
-		
-		Services services = retrofit.create(Services.class);
-		
-		Call<String> result = services.getRunningTasks(workspaceId, userId, true);
-		String response = result.execute().body();
-		
-		tasks = new JSONArray(response);
-		if (tasks.length() > 0) {
-			taskRunning = true;
-			JSONObject runningTask = tasks.getJSONObject(0);
-			taskName = runningTask.getString("description");
-			String taskId = taskName.substring(taskName.indexOf("[") + 1, taskName.indexOf("]"));
-			taskLink = "https://uffiziojira.atlassian.net/browse/" + taskId;
-		} else {
-			taskRunning = false;
-			taskName = "Stopped";
-			taskLink = "";
-		}
-		
-		SimpleDateFormat sdfSQL = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-		sdfSQL.setTimeZone(TimeZone.getTimeZone(userTimeZone));
-		
-		String userDayStartTime = sdfSQL.format(System.currentTimeMillis()).split(" ")[0] + "T00:00:00.000Z";
-		result = services.getTotalWorkDuration(workspaceId, userId, userDayStartTime);
-		response = result.execute().body();
-		tasks = new JSONArray(response);
-		JSONObject task;
-		String taskDuration;
-		Duration duration;
-		long workDurationMillis = 0;
-		
-		SimpleDateFormat sdfISO = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
-		sdfISO.setTimeZone(TimeZone.getTimeZone("UTC"));
-		
-		long estimatedWorkHourInMinute = 8 * 60;
-		for (int i = 0; i < tasks.length(); i++) {
-			task = tasks.getJSONObject(i);
-			if (task.getJSONObject("timeInterval").isNull("duration")) {
-				workDurationMillis += 
-						System.currentTimeMillis() 
-						- sdfISO.parse(task.getJSONObject("timeInterval").getString("start")).getTime();
+		try {
+			boolean taskRunning;
+			String taskName;
+			String taskLink;
+			JSONArray tasks;
+			
+			Services services = retrofit.create(Services.class);
+			
+			Call<String> result = services.getRunningTasks(workspaceId, userId, true);
+			String response = result.execute().body();
+			
+			tasks = new JSONArray(response);
+			if (tasks.length() > 0) {
+				taskRunning = true;
+				JSONObject runningTask = tasks.getJSONObject(0);
+				taskName = runningTask.getString("description");
+				String taskId = taskName.substring(taskName.indexOf("[") + 1, taskName.indexOf("]"));
+				taskLink = "https://uffiziojira.atlassian.net/browse/" + taskId;
 			} else {
-				taskDuration = task.getJSONObject("timeInterval").getString("duration");
-				duration = Duration.parse(taskDuration);
-				workDurationMillis += duration.toMillis();
+				taskRunning = false;
+				taskName = "Stopped";
+				taskLink = "";
 			}
+			
+			SimpleDateFormat sdfSQL = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+			sdfSQL.setTimeZone(TimeZone.getTimeZone(userTimeZone));
+			
+			String userDayStartTime = sdfSQL.format(System.currentTimeMillis()).split(" ")[0] + "T00:00:00.000Z";
+			result = services.getTotalWorkDuration(workspaceId, userId, userDayStartTime);
+			response = result.execute().body();
+			tasks = new JSONArray(response);
+			JSONObject task;
+			String taskDuration;
+			Duration duration;
+			long workDurationMillis = 0;
+			
+			SimpleDateFormat sdfISO = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+			sdfISO.setTimeZone(TimeZone.getTimeZone("UTC"));
+			
+			long estimatedWorkHourInMinute = 8 * 60;
+			for (int i = 0; i < tasks.length(); i++) {
+				task = tasks.getJSONObject(i);
+				if (task.getJSONObject("timeInterval").isNull("duration")) {
+					workDurationMillis += 
+							System.currentTimeMillis() 
+							- sdfISO.parse(task.getJSONObject("timeInterval").getString("start")).getTime();
+				} else {
+					taskDuration = task.getJSONObject("timeInterval").getString("duration");
+					duration = Duration.parse(taskDuration);
+					workDurationMillis += duration.toMillis();
+				}
+			}
+			int workDurationMinute = (int) (workDurationMillis / (1000 * 60));
+			int hour = workDurationMinute / 60;
+			int minute = workDurationMinute % 60;
+			
+			int remainingHour = (int) (Math.abs(estimatedWorkHourInMinute - workDurationMinute) / 60);
+			int remainingMinute = (int) (Math.abs(estimatedWorkHourInMinute - workDurationMinute) % 60);
+			String remainingDurationSign = estimatedWorkHourInMinute - workDurationMinute > 0 ? "-" : "+";
+			
+			String workDuration = String.format("%02d:%02d (" + remainingDurationSign + "%02d:%02d)", hour, minute, remainingHour, remainingMinute);
+			
+			printContent(taskName, taskLink, workDuration, taskRunning);
+		} catch (Exception e) {
+			printContent("", "", "Error", false);
 		}
-		int workDurationMinute = (int) (workDurationMillis / (1000 * 60));
-		int hour = workDurationMinute / 60;
-		int minute = workDurationMinute % 60;
-		
-		int remainingHour = (int) (Math.abs(estimatedWorkHourInMinute - workDurationMinute) / 60);
-		int remainingMinute = (int) (Math.abs(estimatedWorkHourInMinute - workDurationMinute) % 60);
-		String remainingDurationSign = estimatedWorkHourInMinute - workDurationMinute > 0 ? "-" : "+";
-		
-		String workDuration = String.format("%02d:%02d (" + remainingDurationSign + "%02d:%02d)", hour, minute, remainingHour, remainingMinute);
-		
-		printContent(taskName, taskLink, workDuration, taskRunning);
 	}
 	
 	public void printContent(String taskName, String taskLink, String workDuration, boolean taskRunning) {
